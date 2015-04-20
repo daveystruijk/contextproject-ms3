2f04eb97df934f4c82f132b8203ac6700929d2cc
Davey Struijk <mail@daveystruijk.com>
2015-04-20 18:22:52 +0200

    Committed a method with code style errors

--- contextproject-ms3/src/main/java/contextproject/App.java
+++ contextproject-ms3/src/main/java/contextproject/App.java
@@ -8,4 +8,11 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
+  
+  public void codeStyleWarnings() {
+    int x = 5; 
+    x = 5; // this is not how code should look like
+    
+  int y = 12345;
+  }
 }
