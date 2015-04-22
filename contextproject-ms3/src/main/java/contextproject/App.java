4de35dd35a8bd3cecd0ab4c4c1639b9374a5b148
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-04-22 14:36:41 +0200

    Implemented file reading in CLI

--- contextproject-ms3/src/main/java/contextproject/App.java
+++ contextproject-ms3/src/main/java/contextproject/App.java
@@ -9,9 +9,9 @@ import controller.Clicontroller;
 public class App {
   /**
    * Should start our application. No GUI for now just CLI.
-   * @param args: Arguments for main method
    */
   public static void main(String[] args) {
-    Clicontroller.run(); 
+    Clicontroller control = new Clicontroller();
+    control.run(); 
   }
 }
