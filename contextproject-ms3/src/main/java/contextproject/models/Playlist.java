67493c2a5faa9f73e2aa709c716f3d1f7a8e6af1
Roy-Graafmans <R.S.Graafmans@student.tudelft.nl>
2015-04-22 14:12:31 +0200

    Base structure for a playlist

---
+++ contextproject-ms3/src/main/java/contextproject/models/Playlist.java
@@ -0,0 +1,26 @@
+package contextproject.models;
+
+import java.util.ArrayList;
+
+public class Playlist extends ArrayList<Track> {
+
+  /**
+   * serialVersionUID.
+   */
+  private static final long serialVersionUID = 1L;
+  
+  private ArrayList<Track> pl;
+
+  
+  /**
+   * Generate an ArrayList with tracks.
+   * @param songs ArrayList with absolute path
+   */
+  public Playlist(ArrayList<String> songs) {
+    for (String song : songs) {
+      Track tr = new Track(song);
+      pl.add(tr);
+    }
+  }
+  
+}
