4e54486ac8b006650eca34e887609149b7ec1c73
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-04-22 14:07:03 +0200

    Implemented about-devs page in CLI

--- contextproject-ms3/src/main/java/contextproject/App.java
+++ contextproject-ms3/src/main/java/contextproject/App.java
@@ -1,12 +1,17 @@
 package contextproject;
 
+import controller.Clicontroller;
+
 /**
  * Hello world.
  *
  */
 public class App {
+  /**
+   * Should start our application. No GUI for now just CLI.
+   * @param args: Arguments for main method
+   */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("test test test");
+    Clicontroller.run(); 
   }
 }
