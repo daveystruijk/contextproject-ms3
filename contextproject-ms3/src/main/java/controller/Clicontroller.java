b63b1b6ee00b31ed1cb9b0bf98e1b9e5b079ade1
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-04-23 11:06:07 +0200

    Fixed tracking issues

---
+++ contextproject-ms3/src/main/java/controller/Clicontroller.java
@@ -0,0 +1,71 @@
+package controller;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Clicontroller {
+
+  private String fileName;
+  /**
+   * This will start a simple Command Line Interface for our first working version. Optionally to be
+   * included in later versions.
+   */
+  public void run() {
+    int input = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Welcome to our application!");
+    while (input != 3) {
+      System.out.println("Option 1: Construct playlist!");
+      System.out.println("Option 2: About");
+      System.out.println("Option 3: Close");
+      input = sc.nextInt();
+
+      switch (input) {
+        case 1 :
+          try {
+            this.setFileName(sc.next());
+          } catch (IOException e) {
+            System.out.println(e.getMessage());
+          }
+          break;
+        case 2 :
+          System.out.println(this.aboutDevs());
+          break;
+        default :
+          break;
+      }
+    }
+    sc.close();
+  }
+  /**
+   * This method prints info about the team behind this project.
+   * 
+   * @return a String with info
+   */
+  public String aboutDevs() {
+    return "Hey, we are Davey, Felix, Emiel, Roy and Millen!\n"
+        + "We are trying to create a cool product that could serve as the future of music\n"
+        + "Not to sound overly ambitious or something...\n" + "Remember stay hungry, stay foolish!";
+  }
+
+  /**
+   * This takes up the name of the music file to be read into the system.
+   * 
+   * @param input
+   *          Name of the file.
+   * @throws IOException
+   *           : To be thrown when name is invalid.
+   */
+  public void setFileName(String input) throws IOException {
+
+    if (fileName == null || fileName.equals("")) {
+      throw new IOException("The name of the file is invalid!");
+    } else {
+      fileName = input;
+    }
+  }
+
+  public String getFileName() {
+    return this.fileName;
+  }
+}
