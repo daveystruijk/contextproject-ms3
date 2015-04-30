4ae8789c8a63b175ca79adf948aa671cd529ba89
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-04-30 11:08:03 +0200

    Finished CLIController

--- contextproject-ms3/src/main/java/contextproject/controllers/CLIController.java
+++ contextproject-ms3/src/main/java/contextproject/controllers/CLIController.java
@@ -3,7 +3,6 @@ package contextproject.controllers;
 import contextproject.loaders.FolderLoader;
 import contextproject.models.Playlist;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -13,85 +12,27 @@ import java.util.Scanner;
  */
 public class CLIController {
 
-  private String fileName;
-  private Playlist result;
-
-  /**
-   * Input is initialised to empty string and result to null.
-   */
-  public CLIController() {
-    fileName = "";
-    result = null;
-
-  }
   /**
    * This will start a simple Command Line Interface for our first working version. Optionally to be
    * included in later versions.
    */
   public void run() {
-    int input = 0;
     Scanner sc = new Scanner(System.in);
     System.out.println("Welcome to our application!");
-    while (input != 4) {
-      System.out.println("Option 1: Construct playlist! Enter foldername");
-      System.out.println("Option 2: About");
-      System.out.println("Option 3: Play music!");
-      System.out.println("Option 4: Close");
-      input = sc.nextInt();
+    while (true) {
+      System.out.println("Construct playlist! Enter foldername");
 
-      switch (input) {
-        case 1 :
-          try {
-            this.setFileName(sc.nextLine());
-            FolderLoader folderLoader = new FolderLoader(this.getFileName());
-            this.result = new Playlist(folderLoader.load());
-          } catch (Exception e) {
-            System.out.println(e.getMessage());
-          }
-          break;
-        case 2 :
-          System.out.println(this.aboutDevs());
-          break;
-        case 3 :
-          System.out.println("Let's play some music!");
-          break;
-        default :
-          break;
+      try {
+        String fileName = sc.next();
+        FolderLoader folderLoader = new FolderLoader(fileName);
+        Playlist result = new Playlist(folderLoader.load());
+        //TODO!
+        System.out.println("\\TODO!");
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
       }
+      sc.close();
+
     }
-    sc.close();
-  }
-  /**
-   * This method prints info about the team behind this project.
-   * 
-   * @return a String with info
-   */
-  public String aboutDevs() {
-    return "Hey, we are Davey, Felix, Emiel, Roy and Millen!\n"
-        + "We are trying to create a cool product that could serve as the future of music\n"
-        + "Not to sound overly ambitious..\n" + "Remember stay hungry, stay foolish!";
-  }
-
-  /**
-   * This takes up the name of the music file to be read into the system.
-   * 
-   * @param input :Name of the file.
-   * @throws IOException: To be thrown when name is invalid.
-   */
-  public void setFileName(String input) throws IOException {
-
-    if (input == null || input.length() == 0) {
-      throw new IOException("The name of the file is invalid!");
-    } else {
-      fileName = input;
-    }
-  }
-
-  public String getFileName() {
-    return this.fileName;
-  }
-
-  public Playlist getTrackOrdering() {
-    return this.result;
   }
 }
