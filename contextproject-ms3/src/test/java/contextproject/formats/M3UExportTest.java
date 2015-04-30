02f720c28e53ad30395ea2f6c7328de7924f452f
Emiel Rietdijk <emielrietdijk@kabelfoon.net>
2015-04-30 11:58:11 +0200

    Removed unused file and removed a checkstyle warning.

--- contextproject-ms3/src/test/java/contextproject/formats/M3UExportTest.java
+++ contextproject-ms3/src/test/java/contextproject/formats/M3UExportTest.java
@@ -2,6 +2,7 @@ package contextproject.formats;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+
 import contextproject.formats.M3UBuilder;
 import contextproject.models.Playlist;
 
