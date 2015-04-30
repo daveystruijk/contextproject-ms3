66f2c124c0b291d661b28584ec3ee2665ef21101
Millen van Osch <Millen@wlan-145-94-141-247.wlan.tudelft.nl>
2015-04-30 11:36:08 +0200

    manual test for logger functionality

--- contextproject-ms3/src/main/java/contextproject/App.java
+++ contextproject-ms3/src/main/java/contextproject/App.java
@@ -10,9 +10,10 @@ import org.apache.logging.log4j.Logger;
 public class App {
   static Logger elog = LogManager.getLogger("Error-log");
   static Logger dlog = LogManager.getLogger("Debug-log");
+  static Logger log = LogManager.getLogger(App.class.getName());
   
   /**
-   * test method for log4j.
+   * test method for log4j2.
    * @param args args.
    */
   public static void main(String[] args) {
@@ -20,11 +21,15 @@ public class App {
     System.out.println("test test test");
     elog.trace("elog trace");
     dlog.trace("dlog trace");
+    log.trace("checking the root logger");
     try {
       throw new NullPointerException();
     } catch (NullPointerException e) {
       elog.error("error in paradise");
       elog.trace(StackTrace.stackTrace(e));
+      log.error("error in paradise");
+      log.trace(StackTrace.stackTrace(e));
+      
     }
   }
 }
