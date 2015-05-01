d7da9967dab775cabd1dee56780b597351aacb77
Davey Struijk <mail@daveystruijk.com>
2015-05-01 09:01:18 +0200

    Updated algorithm to add each track to the mix only once. Also: exporting to m3u

--- contextproject-ms3/src/main/java/contextproject/controllers/CLIController.java
+++ contextproject-ms3/src/main/java/contextproject/controllers/CLIController.java
@@ -1,7 +1,14 @@
 package contextproject.controllers;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
+import contextproject.formats.M3UBuilder;
 import contextproject.helpers.TrackCompatibility;
 import contextproject.loaders.FolderLoader;
 import contextproject.models.Playlist;
@@ -20,32 +27,63 @@ public class CLIController {
    */
   public void run() {
     Scanner sc = new Scanner(System.in);
-    while (true) {
-      System.out.println("Construct optimal playlist! Enter directory:");
 
-      String fileName = sc.nextLine();
-      FolderLoader folderLoader = new FolderLoader(fileName);
-      Playlist playlist = new Playlist(folderLoader.load());
-      
-      for (Track i : playlist) {
-        Track bestMatch = null;
-        float bestScore = 0.0f;
-        for (int j = 0; j < playlist.size(); j++) {
-          if (i != playlist.get(j)) {
-            float score = TrackCompatibility.getScore(i, playlist.get(j));
-            if (score > bestScore) {
-              bestScore = score;
-              bestMatch = playlist.get(j);
-            }
+    System.out.println("Construct optimal playlist! Enter directory:");
+
+    String fileName = sc.nextLine();
+    sc.close();
+    FolderLoader folderLoader = new FolderLoader(fileName);
+    Playlist playlist = new Playlist(folderLoader.load());
+    ArrayList<Track> addedTracks = new ArrayList<Track>();
+    Playlist mixablePlaylist = new Playlist();
+    
+    Track currentTrack = playlist.get(0);
+    mixablePlaylist.add(currentTrack);
+    while (currentTrack != null) {
+      addedTracks.add(currentTrack);
+      Track bestMatch = null;
+      float bestScore = 0.0f;
+      for (int j = 0; j < playlist.size(); j++) {
+        if (currentTrack != playlist.get(j)) {
+          float score = TrackCompatibility.getScore(currentTrack, playlist.get(j));
+          if (score > bestScore && !addedTracks.contains(playlist.get(j))) {
+            bestScore = score;
+            bestMatch = playlist.get(j);
           }
         }
-        System.out.println();
-        printTrack(i);
-        printTrack(bestMatch);
-        System.out.println(bestScore);
-        System.out.println();
       }
+      if (bestMatch == null) {
+        break;
+      }
+      
+      System.out.println();
+      printTrack(currentTrack);
+      printTrack(bestMatch);
+      System.out.println(bestScore);
+      System.out.println();
+      
+      mixablePlaylist.add(bestMatch);
+      currentTrack = bestMatch;
     }
+    
+    System.out.println("Done! Calculated mixable playlist through "
+        + addedTracks.size() + " tracks.");
+    
+    M3UBuilder builder = new M3UBuilder(mixablePlaylist);
+    String name = new SimpleDateFormat("yyyy-MM-dd hh-mm-ss'.m3u'").format(new Date());
+    String dirPath = System.getProperty("user.dir").toString() + File.separator 
+        + "playlists";
+    new File(dirPath).mkdirs();
+    String fullPath = dirPath + File.separator + name;
+    try {
+      FileWriter writer = new FileWriter(fullPath);
+      writer.write(builder.build());
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    System.out.println("Exported to playlists" + fullPath);
   }
   
   public void printTrack(Track track) {
