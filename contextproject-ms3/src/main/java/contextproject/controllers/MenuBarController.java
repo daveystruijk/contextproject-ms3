3ba2cd004ef802c791b6c25af5af6fcb0d11c874
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-05-05 18:39:42 +0200

    Started work on MenuBar

---
+++ contextproject-ms3/src/main/java/contextproject/controllers/MenuBarController.java
@@ -0,0 +1,15 @@
+package contextproject.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+public class MenuBarController {
+  @FXML
+  private Menu menu;
+  @FXML
+  private MenuBar menuBar;
+  @FXML
+  private MenuItem menuItem;
+}
