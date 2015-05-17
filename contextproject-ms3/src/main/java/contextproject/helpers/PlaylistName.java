1cfc11dfbac05e0a07d5aca5d1891728c33a8f35
MilleIT <m.j.e.vanosch@student.tudelft.nl>
2015-05-17 15:00:11 +0200

    wrote a playlistname universal test

--- contextproject-ms3/src/main/java/contextproject/helpers/PlaylistName.java
+++ contextproject-ms3/src/main/java/contextproject/helpers/PlaylistName.java
@@ -1,16 +1,18 @@
 package contextproject.helpers;
 
+import java.io.File;
+
 public class PlaylistName {
   /**
    * get the name of the folder.
    * 
    * @param path
-   *          the ablosute path of the folder.
+   *          the absolute path of the folder.
    * @return the name of the folder.
    */
   public static String getName(String path) {
     String[] temp;
-    String delimiter = "/";
+    String delimiter = File.separator;
     temp = path.split(delimiter);
     int size = temp.length;
     String name = temp[(size - 1)];
