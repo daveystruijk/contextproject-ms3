fc010ffe731c42d09bca379c7bcf780f8c919c7c
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-05-19 11:43:03 +0200

    Fully tested Property class

--- contextproject-ms3/src/main/java/contextproject/models/Property.java
+++ contextproject-ms3/src/main/java/contextproject/models/Property.java
@@ -5,22 +5,23 @@ import java.io.Serializable;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-public class Property implements Serializable{
+public class Property implements Serializable {
 
+  private static final long serialVersionUID = 3236190461451926648L;
   private final StringProperty name = new SimpleStringProperty();
-  
-  public Property(String name){
+
+  public Property(String name) {
     this.name.set(name);
   }
-  
+
   public final StringProperty nameProperty() {
     return this.name;
   }
-     
+
   public final String getName() {
     return this.nameProperty().get();
   }
-     
+
   public final void setName(String name) {
     this.nameProperty().set(name);
   }
