dd0c6b1afe8d1a74f93dac135d8b95f91c68d918
MilleIT <m.j.e.vanosch@student.tudelft.nl>
2015-05-26 17:41:37 +0200

    finally fixed the current and next track displays

--- contextproject-ms3/src/main/java/contextproject/controllers/PlayerControlsController.java
+++ contextproject-ms3/src/main/java/contextproject/controllers/PlayerControlsController.java
@@ -5,12 +5,26 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 public class PlayerControlsController {
   @FXML
   private Button playButton;
   @FXML
   private Slider slider;
+  @FXML
+  public TextField currentTrack;
+  @FXML
+  public TextField nextTrack;
+
+  /**
+   * . initialize the controller
+   */
+  public void initialize() {
+    currentTrack.setEditable(false);
+    nextTrack.setEditable(false);
+    togglePlayPause();
+  }
 
   /**
    * Toggles the button.
@@ -29,5 +43,14 @@ public class PlayerControlsController {
       }
     });
   }
-
+  /**
+   * updates the statusbar.
+   * 
+   * @param title
+   *          the title of the current song.
+   */
+  public void update(String curtitle, String nxtitle) {
+    currentTrack.setText(curtitle);
+    nextTrack.setText(nxtitle);
+  }
 }
