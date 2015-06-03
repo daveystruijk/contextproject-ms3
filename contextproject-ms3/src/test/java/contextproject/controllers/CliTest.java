2623c3f679a8c7e28803fc09d42f4b69746567dc
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-06-03 14:15:32 +0200

    New attempt to fix travis error, compiler problems

--- contextproject-ms3/src/test/java/contextproject/controllers/CliTest.java
+++ contextproject-ms3/src/test/java/contextproject/controllers/CliTest.java
@@ -4,11 +4,12 @@ import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
-public class CliControllerTest {
+public class CliTest {
 
   @Test
   public void test() {
-    CliController controller = new CliController();
-    assertNotEquals(controller, null);
+    CliController cli = new CliController();
+    assertNotEquals(cli, null);
   }
+
 }
