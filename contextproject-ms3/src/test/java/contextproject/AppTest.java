2e8a2ceb613979e59e9009909fa55b3a4fa445f1
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-06-04 12:29:53 +0200

    Converted AppTest to an FXTest, see if it holds this time

--- contextproject-ms3/src/test/java/contextproject/AppTest.java
+++ contextproject-ms3/src/test/java/contextproject/AppTest.java
@@ -3,17 +3,26 @@ package contextproject;
 import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import javafx.stage.Stage;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class AppTest extends ApplicationTest {
 
   @Test
   public void appTest() throws Exception {
 
     App ap = new App();
     assertNotEquals(ap, null);
+
+  }
+
+  @Override
+  public void start(Stage arg0) throws Exception {
+    // TODO Auto-generated method stub
 
   }
 
