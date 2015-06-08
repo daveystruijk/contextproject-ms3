1cb470238c923fd5e5a0c6d4d8ea6a5acc0e654a
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-06-08 11:47:16 +0200

    New attempt for TestFX appTest

--- contextproject-ms3/src/test/java/contextproject/AppTest.java
+++ contextproject-ms3/src/test/java/contextproject/AppTest.java
@@ -4,18 +4,33 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import javafx.stage.Stage;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class AppTest extends ApplicationTest {
 
   @Test
   public void appTest() throws Exception {
 
     App ap = new App();
     assertNotEquals(ap, null);
-    assertEquals(App.getController(), null);
+    assertNotEquals(App.getController(), null);
+
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    // TODO Auto-generated method stub
+    App ap = new App();
+    ap.start(stage);
+  }
+
+  @Test
+  public void typeTest() {
 
   }
 }
