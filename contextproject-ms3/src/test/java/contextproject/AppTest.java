92940e645d602f27d8be3b87566555cd5ec0a1ca
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-06-08 12:42:43 +0200

    Different approach now, no direct call to app.start

--- contextproject-ms3/src/test/java/contextproject/AppTest.java
+++ contextproject-ms3/src/test/java/contextproject/AppTest.java
@@ -3,9 +3,13 @@ package contextproject;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -25,10 +29,22 @@ public class AppTest extends ApplicationTest {
   @Override
   public void start(Stage stage) throws Exception {
     // TODO Auto-generated method stub
-    App ap = new App();
-    ap.start(stage);
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/window.fxml"));
+
+    Parent root = (Parent) loader.load();
+
+    Scene scene = new Scene(root, 1200, 800);
+    stage.setTitle("Window Test!");
+    stage.setScene(scene);
+    stage.show();
+
   }
 
+  @Before
+  public void setUp() {
+    
+  }
+  
   @Test
   public void typeTest() {
     clickOn("#playButton");
