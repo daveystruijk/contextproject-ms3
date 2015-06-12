e0a43560a402ec2905589fd2dae501266b69767e
FelixvanDoorn <felix.a.vandoorn@gmail.com>
2015-06-12 23:58:41 +0200

    Added SE-deliverables

---
+++ contextproject-ms3/src/test/java/contextproject/audio/transitions/BaseTransitionTest.java
@@ -0,0 +1,32 @@
+package contextproject.audio.transitions;
+
+import contextproject.audio.TrackProcessor;
+import contextproject.audio.transitions.BaseTransition.TransitionDoneCallback;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class BaseTransitionTest {
+
+  private TrackProcessor from;
+  private TrackProcessor to;
+  private TransitionDoneCallback callback;
+
+  @Before
+  public void setUp() {
+    from = Mockito.mock(TrackProcessor.class);
+    to = Mockito.mock(TrackProcessor.class);
+    callback = Mockito.mock(TransitionDoneCallback.class);
+
+  }
+  @Test
+  public void test() {
+    BaseTransition base = new FadeInOutTransition(from, to, callback);
+    base.begin(from, to);
+    assertEquals(base.getClass(), FadeInOutTransition.class);
+  }
+
+}
